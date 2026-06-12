9f2fdaac6b587a533512ad73f2bc35ee01f1a679
Jeffery Unah <Jefferyunah@gmail.com>
2026-06-12 12:23:34 +0100

    Update Javascript.js

--- script/Javascript.js
+++ script/Javascript.js
@@ -1,1 +1,7 @@
+document.getElementById("bookingForm")
+.addEventListener("submit", function(event){
 
+    event.preventDefault();
+
+    alert("Booking Confirmed! Thank you for choosing Pulse VR.");
+});
